8724b0218aa96f9f2aa59749e980cb09f87f863d
bhavyavarshney-123 <56428385+bhavyavarshney-123@users.noreply.github.com>
2022-07-24 03:53:27 +0530

    Add files via upload
    This is the implementation of base64 Encoding and Decoding without any library import

---
+++ Base64-(Encoding and Decoding)/temp.py
@@ -0,0 +1,11 @@
+Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: B:/Sarva Assignments/Encode-Decode(Base64)/Encoding(Raw).py =====
+Enter a string to convert into base64:Lets Encode
+ TGV0cyBFbmNvZGU=
+>>> 
+===== RESTART: B:/Sarva Assignments/Encode-Decode(Base64)/Decoding(Raw).py =====
+Enter a base64 to convert into string: TGV0cyBFbmNvZGU=
+ Lets Encode
+>>> 
